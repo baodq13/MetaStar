8bb9734adad0c7b008a17e54d4d9b6d313de34bc
baodangvar <baodang@var-meta.com>
2024-07-22 22:58:38 +0700

    create project

---
+++ src/public/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "VAR-META-var_metaverse-0.1";
+const contentToCache = [
+    "Build/Build.loader.js",
+    "Build/Build.framework.js",
+    "https://serverfile.valorfantasy.com/uploads/Build_33a4fa342f.data",
+    "Build/Build.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
